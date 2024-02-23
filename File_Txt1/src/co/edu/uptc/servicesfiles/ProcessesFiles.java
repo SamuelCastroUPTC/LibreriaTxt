b72328d3776521e5546a2dcd5bd5503e4a51dbb0
unknown <castro.samuel@uptc.edu.co>
2024-02-23 11:53:39 -0500

    Libreria de files

---
+++ File_Txt1/src/co/edu/uptc/servicesfiles/ProcessesFiles.java
@@ -0,0 +1,74 @@
+package co.edu.uptc.servicesfiles;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProcessesFiles {
+
+    private BufferedReader bufferedReader;
+    private String fileName;
+    private String charsetName;
+
+    public ProcessesFiles(){
+        this.charsetName="UTF-8";
+    }
+
+    public void setCharName(String charName){
+        this.charsetName=charName;
+    }
+
+    public void setNameFile(String name){
+        this.fileName=name;
+    }
+
+    public List<String> extraerString() throws Exception {
+        this.openFile();
+        List<String> list=this.readFile();
+        this.closedFile();
+        return list;
+    }
+
+    private void openFile() throws Exception{
+        File file= new File(fileName);
+        if (!file.exists()) {
+            throw new Exception("El archivo no existe");
+        }else{
+            Reader reader = new InputStreamReader(new FileInputStream(file),this.charsetName);
+            bufferedReader = new BufferedReader(reader);
+        }
+        
+    }
+
+    private List<String> readFile() throws Exception{
+        String cont="";
+        List<String> list = new ArrayList<String>();
+        while ((cont=bufferedReader.readLine())!=null) {
+            list.add(cont);
+        }
+        return list;
+    }
+
+    private void closedFile() throws Exception{
+        if (bufferedReader!= null) {
+            bufferedReader.close();
+        }
+    }
+
+    // private List<String> changeBase64() {
+    //     String base64="";
+    //     for (String contentbase64 : content) {
+    //         base64=base64+contentbase64;
+    //     }
+    //     byte[] bytesDecoded= Base64.getDecoder().decode(base64);
+    //     String conteded=new String(bytesDecoded);
+    //     List<String> list=content;
+    //     return list;
+    // }
+
+
+}
